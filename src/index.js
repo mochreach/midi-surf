15df957087def349b6ec9307882116ead86ce148
ChrisWellsWood <cwwoodesq@gmail.com>
2021-10-24 01:26:50 +0100

    Feat: Adds basic midi functions.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
-import './main.css';
-import { Elm } from './Main.elm';
-import * as serviceWorker from './serviceWorker';
+import "./main.css";
+import { Elm } from "./Main.elm";
+import * as serviceWorker from "./serviceWorker";
 
 Elm.Main.init({
-  node: document.getElementById('root')
+  node: document.getElementById("root"),
 });
+
+navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
+
+function onMIDISuccess(midiAccess) {
+  console.log("MIDI ready!");
+  var inputs = midiAccess.inputs.values();
+
+  midiAccess.onstatechange = function (e) {
+    console.log(e.port.name, e.port.manufacturer, e.port.state);
+  };
+
+  // midiAccess.inputs.forEach((input) => {
+  //   console.log(input.name); /* inherited property from MIDIPort */
+  //   input.onmidimessage = function (message) {
+  //     console.log(message.data);
+  //   };
+  // });
+}
+
+function onMIDIFailure(msg) {
+  console.log("Failed to get MIDI access - " + msg);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
