9d07b82a87f39249b06697cb2dd25b03afa8390c
Chris Wells Wood <cwwoodesq@gmail.com>
2022-11-26 14:10:16 +0000

    Fix: Checks midi out is defined before sending messages.

--- src/index.js
+++ src/index.js
@@ -1,50 +1,58 @@
 import "./main.css";
-import { Elm } from "./Main.elm";
+import {Elm} from "./Main.elm";
 import * as serviceWorker from "./serviceWorker";
 
 const app = Elm.Main.init({
-  node: document.getElementById("root"),
+    node: document.getElementById("root"),
 });
 let midiOut = null;
 
-app.ports.sendNoteOn.subscribe(function(noteNumber) {
-    midiOut.send([0x95, noteNumber, 0x7f]);
-    console.log("On", noteNumber);
+app.ports.sendNoteOn.subscribe(function (noteNumber) {
+    if (midiOut) {
+        midiOut.send([0x95, noteNumber, 0x7f]);
+        console.log("On", noteNumber);
+    } else {
+        console.log("midiOut undefined: Cannot send note off.");
+    }
 });
 
-app.ports.sendNoteOff.subscribe(function(noteNumber) {
-    midiOut.send([0x85, noteNumber, 0x7f]);
-    console.log("Off", noteNumber);
+app.ports.sendNoteOff.subscribe(function (noteNumber) {
+    if (midiOut) {
+        midiOut.send([0x85, noteNumber, 0x7f]);
+        console.log("Off", noteNumber);
+    } else {
+        console.log("midiOut undefined: Cannot send note off.");
+    }
 });
 
 navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
 
 function onMIDISuccess(midiAccess) {
-  console.log("MIDI ready!");
-  app.ports.listenForMIDIStatus.send(true);
+    console.log("MIDI ready!");
+    app.ports.listenForMIDIStatus.send(true);
 
-  let outputs = Array.from(midiAccess.outputs.values());
-  console.log(outputs);
-  
-  // this should be changed to outputs.get("name")
-  midiOut = outputs[2];
-  console.log(midiOut);
+    let outputs = Array.from(midiAccess.outputs.values());
+    console.log(outputs);
 
-  midiAccess.onstatechange = function (e) {
-    console.log(e.port.name);
-  };
+    // this should be changed to outputs.get("name")
+    midiOut = outputs[2];
+    console.log(midiOut);
 
-  // midiAccess.inputs.forEach((input) => {
-  //   console.log(input.name); /* inherited property from MIDIPort */
-  //   input.onmidimessage = function (message) {
-  //     console.log(message.data);
-  //   };
-  // });
+    midiAccess.onstatechange = function (e) {
+        console.log(e.port.name);
+    };
+
+    // midiAccess.inputs.forEach((input) => {
+    //   console.log(input.name); /* inherited property from MIDIPort */
+    //   input.onmidimessage = function (message) {
+    //     console.log(message.data);
+    //   };
+    // });
 }
 
 function onMIDIFailure(msg) {
-  app.ports.listenForMIDIStatus.send(false);
-  console.log("Failed to get MIDI access - " + msg);
+    app.ports.listenForMIDIStatus.send(false);
+    console.log("Failed to get MIDI access - " + msg);
 }
 
 // If you want your app to work offline and load faster, you can change
