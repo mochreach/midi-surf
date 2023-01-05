546dcf493375c880bd4b798bd96821177213d1a4
Chris Wells Wood <cwwoodesq@gmail.com>
2023-01-05 22:09:45 +0000

    Build

---
+++ docs/precache-manifest.ddfd8d2c420acc7e8b096cec9f380ffe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19dd9a2e6536b4d07b786548d9396094",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "de207699927c00e4f351a151e9569d39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "86913277c95463bb32d0",
+    "url": "/static/css/main.0af5c45f.chunk.css"
+  },
+  {
+    "revision": "86913277c95463bb32d0",
+    "url": "/static/js/main.86913277.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "354e092f0aa2734f8f4c",
+    "url": "/static/js/vendors~main.354e092f.chunk.js"
+  }
+]);
